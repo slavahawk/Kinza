daeff3baa022eeeb7d4872b2825b73cbac5950e6
slavahawk <krylov_s10@mail.ru>
2020-06-26 21:28:24 +0700

    Замена картинок, добвил в фильтр ссылки, адаптив корзины и продукта

--- #src/js/script.js
+++ #src/js/script.js
@@ -7,23 +7,3 @@
 @@include('flyToBasket.js');
 
 new WOW().init();
-$(function () {
-	(function quantityProducts() {
-		var $quantityArrowMinus = $('.quantity-arrow-minus');
-		var $quantityArrowPlus = $('.quantity-arrow-plus');
-		var $quantityNum = $('.quantity-num');
-
-		$quantityArrowMinus.click(quantityMinus);
-		$quantityArrowPlus.click(quantityPlus);
-
-		function quantityMinus() {
-			if ($quantityNum.val() > 1) {
-				$quantityNum.val(+$quantityNum.val() - 1);
-			}
-		}
-
-		function quantityPlus() {
-			$quantityNum.val(+$quantityNum.val() + 1);
-		}
-	})();
-});
--- kinza/js/script.js
+++ kinza/js/script.js
@@ -754,23 +754,3 @@ $(document).ready(function () {
 ;
 
 new WOW().init();
-$(function () {
-	(function quantityProducts() {
-		var $quantityArrowMinus = $('.quantity-arrow-minus');
-		var $quantityArrowPlus = $('.quantity-arrow-plus');
-		var $quantityNum = $('.quantity-num');
-
-		$quantityArrowMinus.click(quantityMinus);
-		$quantityArrowPlus.click(quantityPlus);
-
-		function quantityMinus() {
-			if ($quantityNum.val() > 1) {
-				$quantityNum.val(+$quantityNum.val() - 1);
-			}
-		}
-
-		function quantityPlus() {
-			$quantityNum.val(+$quantityNum.val() + 1);
-		}
-	})();
-});
