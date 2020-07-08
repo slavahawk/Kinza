023d5546411b2a819249206e7d4e092c376d1914
slavahawk <krylov_s10@mail.ru>
2020-07-08 18:58:45 +0700

    анимация овощей на мобилке и пк

--- kinza/js/script.js
+++ kinza/js/script.js
@@ -478,7 +478,7 @@ $('body').on('mousemove', (e) => {
 	const x = e.pageX / $(window).width();
 	const y = e.pageY / $(window).height();
 
-	$('.list1, .list2, .list3, .list4, .list5, .list6').css(
+	$('.list1, .list2, .list3, .list4, .list5, .list6, .list7, .list8 ').css(
 		'transform',
 		'translate(' + x * 30 + 'px, ' + y * 30 + 'px)'
 	);
