95f25b79e63d55e9a10cd0d9353964cfb9c0b1df
Alexey <info@webskill24.ru>
2020-09-05 12:33:40 +0300

    Env 1.0

--- #src/js/filter.js
+++
@@ -1,7 +0,0 @@
-$(function () {
-	'use strict';
-	$('#buttonFilterMini, .title__box-mini').click(function () {
-		$('.filter__box-mini').toggleClass('open');
-		$('body').toggleClass('lock');
-	});
-});
--- #src/js/script.js
+++
@@ -1,8 +0,0 @@
-@@include('webp.js');
-@@include('burger.js');
-@@include('filter.js');
-@@include('list.js');
-@@include('map.js');
-@@include('flyToBasket.js');
-@@include('payButton.js');
-@@include('tab.js');
