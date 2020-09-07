b0ae165b797345b88c2bfc4feb357ce9e4434508
slavahawk <krylov_s10@mail.ru>
2020-09-07 23:06:20 +0700

    js

--- js/script.js
+++ js/script.js
@@ -325,6 +325,60 @@ if (tab) {
   setIndex();
 }
 
+var maskPhone = function maskPhone() {
+  var inputsPhone = document.querySelectorAll("input[name=\"Order[phone]\"]");
+  inputsPhone.forEach(function (input) {
+    var keyCode;
+
+    var mask = function mask(event) {
+      event.keyCode && (keyCode = event.keyCode);
+      var pos = input.selectionStart;
+
+      if (pos < 3) {
+        event.preventDefault();
+      }
+
+      var matrix = "+7 (___) ___ ____",
+          i = 0,
+          def = matrix.replace(/\D/g, ""),
+          val = input.value.replace(/\D/g, ""),
+          newValue = matrix.replace(/[_\d]/g, function (a) {
+        if (i < val.length) {
+          return val.charAt(i++) || def.charAt(i);
+        } else {
+          return a;
+        }
+      });
+      i = newValue.indexOf("_");
+
+      if (i != -1) {
+        i < 5 && (i = 3);
+        newValue = newValue.slice(0, i);
+      }
+
+      var reg = matrix.substr(0, input.value.length).replace(/_+/g, function (a) {
+        return "\\d{1," + a.length + "}";
+      }).replace(/[+()]/g, "\\$&");
+      reg = new RegExp("^" + reg + "$");
+
+      if (!reg.test(input.value) || input.value.length < 5 || keyCode > 47 && keyCode < 58) {
+        input.value = newValue;
+      }
+
+      if (event.type == "blur" && input.value.length < 5) {
+        input.value = "";
+      }
+    };
+
+    input.addEventListener("input", mask, false);
+    input.addEventListener("focus", mask, false);
+    input.addEventListener("blur", mask, false);
+    input.addEventListener("keydown", mask, false);
+  });
+};
+
+maskPhone();
+
 var show = function show(e) {
   var t = e.target;
   if (!t.classList.contains('tab__title-btn')) return;
