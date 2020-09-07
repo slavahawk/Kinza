2846b59814fd909c0fe44b4a7fa81d7cc3402082
slavahawk <krylov_s10@mail.ru>
2020-09-07 22:32:59 +0700

    сss

--- js/script.js
+++ js/script.js
@@ -313,3 +313,55 @@ $(document).ready(function () {
   });
 });
 ;
+var tab = document.querySelector('.tabOrder'),
+    tabButton = document.querySelectorAll('.tab__title-btn'),
+    tabContentItem = document.querySelectorAll('.tab__item');
+
+if (tab) {
+  searchFirstElment();
+  tab.addEventListener('click', function (e) {
+    return show(e);
+  });
+  setIndex();
+}
+
+var show = function show(e) {
+  var t = e.target;
+  if (!t.classList.contains('tab__title-btn')) return;
+  deleteActive();
+  var index = t.getAttribute("data-index");
+  var content = document.querySelector('.tab__item[data-index="' + index + '"]');
+  setActive(t, content);
+};
+
+function searchFirstElment() {
+  var tabButton = document.querySelector('.tab__title-btn');
+  var tabContentItem = document.querySelector('.tab__item');
+  tabButton.classList.add('active');
+  tabContentItem.classList.add('active');
+}
+
+;
+
+function deleteActive() {
+  for (var i = 0; i < tabButton.length; i++) {
+    tabButton[i].classList.remove('active');
+    tabContentItem[i].classList.remove('active');
+  }
+}
+
+function setActive(t, content) {
+  for (var i = 0; i < tabButton.length; i++) {
+    t.classList.add("active");
+    content.classList.add("active");
+  }
+}
+
+function setIndex() {
+  for (var i = 0; i < tabButton.length; i++) {
+    tabButton[i].setAttribute("data-index", i);
+    tabContentItem[i].setAttribute("data-index", i);
+  }
+}
+
+;
