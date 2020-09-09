95f35989218c1ee3f0c36badf151c08d3b9b8daa
slavahawk <krylov_s10@mail.ru>
2020-09-10 00:32:38 +0700

    fancybox

--- yii-application/frontend/assets/MainAsset.php
+++ yii-application/frontend/assets/MainAsset.php
@@ -14,11 +14,13 @@ class MainAsset extends AssetBundle
 {
 
     public $css = [
+        'css/libs/jquery.fancybox.min.css',
         'css/style.min.css',
     ];
 
     public $js = [
         'https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js',
+        'js/libs/jquery.fancybox.min.js',
         'js/script.js'
     ];
 
